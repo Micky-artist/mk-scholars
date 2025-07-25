f26b5c75193ffa60cbfd33f012e5bf9489c00408
Remy Hirwa <rhirwa2@huskers.unl.edu>
2025-07-25 23:59:58 +0200

    feat: add driving school page with proper routing and asset handling

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
-<script type="text/javascript">
-	window.location.href="home";
-</script>
+<?php
+// driving-school/index.php
+// Permanent redirect to the driving school page
+header('HTTP/1.1 301 Moved Permanently');
+header('Location: /driving-school/');
+exit;
